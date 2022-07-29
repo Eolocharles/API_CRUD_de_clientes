ca460fa8977a4e70098804fe132b8850e07b65df
Éolo Charles <eolocharles@gmail.com>
2022-07-28 21:14:06 -0300

    Update category

--- backend/src/main/java/com/devsuperior/API_CRUD_de_clients/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/API_CRUD_de_clients/services/ClientService.java
@@ -3,11 +3,12 @@ package com.devsuperior.API_CRUD_de_clients.services;
 import com.devsuperior.API_CRUD_de_clients.DTO.ClientDTO;
 import com.devsuperior.API_CRUD_de_clients.entities.Client;
 import com.devsuperior.API_CRUD_de_clients.repositories.ClientRepository;
-import com.devsuperior.API_CRUD_de_clients.services.exceptions.EntityNotFoundException;
+import com.devsuperior.API_CRUD_de_clients.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -28,7 +29,7 @@ public class ClientService {
     @Transactional(readOnly = true)
     public ClientDTO findById(Long id) {
         Optional<Client> obj = repository.findById(id);
-        Client entity = obj.orElseThrow(() -> new EntityNotFoundException("Objeto não encontrado! Id: " + id));
+        Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Objeto não encontrado! Id: " + id));
         return new ClientDTO(entity);
     }
 
@@ -43,5 +44,21 @@ public class ClientService {
         entity = repository.save(entity);
         return new ClientDTO(entity);
 
+    }
+
+    @Transactional
+    public ClientDTO update(Long id, ClientDTO dto) {
+        try {
+        Client entity = repository.getReferenceById(id);
+        entity.setName(dto.getName());
+        entity.setCpf(dto.getCpf());
+        entity.setIncome(dto.getIncome());
+        entity.setBirthDate(dto.getBirthDate());
+        entity.setChildren(dto.getChildren());
+        entity = repository.save(entity);
+        return new ClientDTO(entity);
+    }catch (EntityNotFoundException e){
+        throw new ResourceNotFoundException("Id not found " + id);
+    }
     }
 }
